f7d4edf7b37a33742eac897913f2b79700c7e8df
Arseniy Merzlyakov <merzliakov.ad@phystech.edu>
2024-04-22 14:19:04 +0300

    5 opt

--- optimisations/src/crc32.asm
+++ optimisations/src/crc32.asm
@@ -2,11 +2,11 @@ section .text
 global asm_get_hash_crc32
 
 asm_get_hash_crc32:
-    mov eax, 0
-    mov r11, 0 
+    xor eax, eax                    ; eax = 0
+    xor r11, r11                    ; r11 = 0
     .loop:
         crc32 eax, byte [rdi+r11]
-        inc r11
+        inc r11                     ; r11++
         cmp r11, rsi
         jne .loop
     ret
